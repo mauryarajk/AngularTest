f65a6bdd93b8a201e3a3caf9a3700c84b8a7b87e
rajkamal1990 <ambuj.iics@gmail.com>
2018-01-11 20:56:15 +0530

    user changes uploaded

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+$url = "http://www.webservicex.net/globalweather.asmx?WSDL";
+$client = new SoapClient($url);
+$fcs = $client->__getFunctions();
+// echo '<pre>';
+var_dump($fcs);
+$param =  array('CountryName' => 'Spain');
+$res = $client->GetCitiesByCountry($param);
+print_r($res);
+// echo $res = $client->getLeadDetails('All');
+
+
+
+
+?>
